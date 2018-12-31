24a8436cdafc80ea3d194d44be831c988dbb807d
madguy02 <manishkakoti688@gmail.com>
2018-12-31 17:44:00 +0530

    Updated code commenting out unnecessary code

--- Object-Loader/js/loaderApp.js
+++ Object-Loader/js/loaderApp.js
@@ -5,13 +5,13 @@ var renderer = new THREE.WebGLRenderer(); // Three.js uses WebGl at the core, a 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement); // plain js to adjust the rendered model
 
-window.addEventListener('resize', function(){
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width/height;
-    camera.updateProjectionMatrix();
-}) // generic event handler for resizing the content window
+// window.addEventListener('resize', function(){
+    // var width = window.innerWidth;
+    // var height = window.innerHeight;
+    // renderer.setSize(width, height);
+    // camera.aspect = width/height;
+    // camera.updateProjectionMatrix();
+// }) // generic event handler for resizing the content window
 
 controls = new THREE.OrbitControls(camera, renderer.domElement); // orbit controls helps to move around the scene
 // initial position of the camera (adjustable as preferred)
